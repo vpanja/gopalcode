fbacd9d37227b21c94a1746d6be0eb8c73cce97f
vpanja <p.venu1123@gmail.com>
2018-08-01 09:11:42 +0530

    more changes

--- check_user.php
+++ check_user.php
@@ -6,116 +6,92 @@
         
         private $db;
         
-        private $db_table = "users";
-        
+        private $db_user_table = "users";
+        private $db_souser_table = "social_users";
+        private $db_sovideo_table = "social_videos";
         public function __construct(){
             $this->db = new DbConnect();
         }
-        
-        public function isLoginExist($username, $password){
-            
-            $query = "select * from ".$this->db_table." where username = '$username' AND password = '$password' Limit 1";
-            
-            $result = mysqli_query($this->db->getDb(), $query);
-            
-            if(mysqli_num_rows($result) > 0){
-                
-                mysqli_close($this->db->getDb());
-                
-                
-                return true;
-                
+        public function addUserVideos($videosDetails){
+            $json = array();
+            $social_query = "insert into ".$this->db_souser_table." (linked_social_app,linked_email,social_username,user_id,identifier) values('Google','".$videosDetails['email']."','".$videosDetails['name']."','".$videosDetails['user_id']."','".$videosDetails['id']."')";
+            mysqli_query($this->db->getDb(), $social_query);
+            if($videosDetails['videoslist']){
+            $video_query = "insert into ".$this->db_sovideo_table." (user_id,channel_id,video_id,created_at)values";
+            foreach($videosDetails['videoslist'] as $videos){
+                if(isset($videos['id']['videoId'])){
+                    $video_query .= "(".$videosDetails['user_id'].",'".$videosDetails['channel_id']."','".$videos['id']['videoId']."',now()),";
+                }
             }
-            
-            mysqli_close($this->db->getDb());
-            
-            return false;
-            
+            $inserted =    mysqli_query($this->db->getDb(), trim($video_query,',')); 
+              $json['message'] = ($inserted)? 'Vidoes Stored Successfully':'Failed to store Data';
+           }else{
+            $json['message'] = 'No videos to Store';
+           }
+            return $json;
         }
-        
-        public function isEmailUsernameExist($username, $email){
-            
-            $query = "select * from ".$this->db_table." where username = '$username' AND email = '$email'";
-            
+        public function isLoginExist($userData){
+            $query = "select id,user_name,user_email,user_status from ".$this->db_user_table." where user_name = '".$userData['name']."' AND user_pass = '".$userData['password']."' Limit 1";
             $result = mysqli_query($this->db->getDb(), $query);
-            
-            if(mysqli_num_rows($result) > 0){
-                
-                mysqli_close($this->db->getDb());
-                
-                return true;
-                
-            }
-               
-            return false;
-            
+            $userDetails=mysqli_fetch_assoc($result);
+            return (mysqli_num_rows($result) > 0 ) ? $userDetails : false;
         }
         
-        public function isValidEmail($email){
-            return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+        public function isEmailUsernameExist($userData){            
+            $query = "select * from ".$this->db_user_table." where user_name = '".$userData['name']."' AND user_email = '".$userData['email']."'";            
+            $result = mysqli_query($this->db->getDb(), $query);            
+            return (mysqli_num_rows($result) > 0 ) ? true : false;            
         }
         
-        public function createNewRegisterUser($username, $password, $email){
-              
-            $isExisting = $this->isEmailUsernameExist($username, $email);
-            
-            if($isExisting){
-                
+        public function isValidEmail($user_email){
+            return filter_var($user_email, FILTER_VALIDATE_EMAIL) !== false;
+        }
+        
+        public function createNewRegisterUser($userData){              
+            $isExisting = $this->isEmailUsernameExist($userData);            
+            if($isExisting){                
                 $json['success'] = 0;
-                $json['message'] = "Error in registering. Probably the username/email already exists";
-            }
-            
-            else{
-                
-            $isValid = $this->isValidEmail($email);
-                
-                if($isValid)
-                {
-                $query = "insert into ".$this->db_table." (username, password, email, created_at, updated_at) values ('$username', '$password', '$email', NOW(), NOW())";
-                
-                $inserted = mysqli_query($this->db->getDb(), $query);
-                
-                if($inserted == 1){
-                    
-                    $json['success'] = 1;
-                    $json['message'] = "Successfully registered the user";
-                    
+                $json['message'] = "Error in registering. Probably the user_name/user_email already exists";
+            }else{                
+                $isValid = $this->isValidEmail($userData['email']);                
+                if($isValid){
+                    $query = "insert into ".$this->db_user_table." (user_name, user_pass, user_email, created_at) values ('".$userData['name']."', '".$userData['password']."', '".$userData['email']."', NOW())";                
+                    $inserted = mysqli_query($this->db->getDb(), $query); 
+                    $user_id = mysqli_insert_id($this->db->getDb());
+                    if($inserted == 1){                    
+                        $json['success'] = 1;
+                        $json['user_id'] = $user_id;
+                        $json['message'] = "Successfully registered the user";                    
+                    }else{                    
+                        $json['success'] = 0;
+                        $json['message'] = "Error in registering. Probably the user_name/user_email already exists";
+                    }
                 }else{
-                    
-                    $json['success'] = 0;
-                    $json['message'] = "Error in registering. Probably the username/email already exists";
-                    
-                }
-                
-                mysqli_close($this->db->getDb());
-                }
-                else{
                     $json['success'] = 0;
                     $json['message'] = "Error in registering. Email Address is not valid";
-                }
-                
-            }
-            
-            return $json;
-            
+                }                
+            }            
+            return $json;            
         }
         
-        public function loginUsers($username, $password){
-            
+        public function loginUsers($userData){
             $json = array();
-            
-            $canUserLogin = $this->isLoginExist($username, $password);
-            
+            $canUserLogin = $this->isLoginExist($userData);
             if($canUserLogin){
-                
                 $json['success'] = 1;
                 $json['message'] = "Successfully logged in";
-                
+                $json['user']=$canUserLogin;
             }else{
                 $json['success'] = 0;
                 $json['message'] = "Incorrect details";
             }
             return $json;
+        }
+        public function getUserVideos($userId){
+            $query = "select video_id from ".$this->db_sovideo_table." where user_id = ".$userId;
+            $result = mysqli_query($this->db->getDb(), $query);
+            $vidoeDetails=mysqli_fetch_assoc($result);
+            return (mysqli_num_rows($result) > 0 ) ? $vidoeDetails : false;
         }
     }
     ?>
--- index.php
+++ index.php
@@ -1,61 +1,118 @@
 <?php
-    
-    require_once 'check_user.php';
-    
-    $username = "";
-    
-    $password = "";
-    
-    $email = "";
-    
-    if(isset($_POST['username'])){
+require_once 'google-api/src/Google/autoload.php';
+require_once 'check_user.php';
+session_start();
+if(isset($_REQUEST['actioncall'])){
+ $action = $_REQUEST['actioncall'];
+  echo ($action != 'logout')? "<a href='index.php?actioncall=logout'>Logout</a><br>":'';
+$userObject = new User();
+$client = new Google_Client();
+$client->setAuthConfigFile('client_secrets.json');
+$client->addScope(array(Google_Service_Oauth2::USERINFO_EMAIL,Google_Service_Oauth2::USERINFO_PROFILE,Google_Service_YouTube::YOUTUBE_UPLOAD,Google_Service_YouTube::YOUTUBE_FORCE_SSL));
+switch ($action) {
+    case 'sociallogin':
+if(isset($_SESSION['access_token']) && $_SESSION['access_token']){
+    $client->setAccessToken($_SESSION['access_token']); 
+    $profile = new Google_Service_Oauth2($client);
+    $profileobject = $profile->userinfo->get();
+    $profileDetails = (array)$profileobject;
+    $videos = addVideos($client);
+    $userFullDetails = array_merge($profileDetails,array('videoslist'=>$videos));
+    $userFullDetails['password'] = md5('stellar'.$userFullDetails['id']);
+    $userFullDetails['channel_id'] = $videos[0]['id']['channelId'];
+    echo '<pre>';
+//    print_r($userFullDetails);exit;
+    $isExisting = $userObject->isEmailUsernameExist($userFullDetails);
+    if($isExisting){
+        $login = $userObject->loginUsers($userFullDetails);
+        echo "Welcome ".$login['user']['user_name']."<br><br><a href='index.php?actioncall=getUserVideos'>Get My Videos</a>";
+        $_SESSION['account']= $login;
+        if($login['user']['id']){
+			$userFullDetails['user_id'] = $login['user']['id'];
+            $socialDetails = $userObject->addUserVideos($userFullDetails);
+            //print_r($socialDetails);
+        }
+       // print_r($_SESSION);
+    }else{                                
+        $register = $userObject->createNewRegisterUser($userFullDetails);
+        //print_r($register);
+        $userFullDetails['user_id'] = $register['user_id'];
+        if($userFullDetails['user_id']){
+            $socialDetails = $userObject->addUserVideos($userFullDetails);
+            //print_r($socialDetails);
+        }
+        $login = $userObject->loginUsers($userFullDetails);
+        echo "Account Created, Welcome ".$login['user']['user_name']."<br><br><a href='index.php?actioncall=getUserVideos'>Get My Videos</a>";
+        $_SESSION['account']= $login;
+     }
+     $_SESSION['account_fulldetails']= $userFullDetails;
+}else{
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/Login/oauth2callback.php';
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
+        break;
+    case 'login':
+            if(!empty($user_name) && !empty($user_pass) && empty($user_email)){   
+                $hashed_user_pass = md5($user_pass);      
+                $json_array = $userObject->loginUsers($user_name, $hashed_user_pass);    
+                echo json_encode($json_array);
+            }
+        break;
+    case 'register':
+            if(!empty($user_name) && !empty($user_pass) && !empty($user_email)){    
+                $hashed_user_pass = md5($user_pass);       
+                $json_registration = $userObject->createNewRegisterUser($user_name, $hashed_user_pass, $user_email);     
+                echo json_encode($json_registration);
+            }
+        break;
+        case 'getUserVideos':
+            if(isset($_SESSION['access_token']) && $_SESSION['access_token'] && isset($_SESSION['account']['user']['id'])){
+                $user_id=$_SESSION['account']['user']['id'];
+                $userVideos = $userObject->getUserVideos($user_id);
+                if($userVideos){
+                    foreach($userVideos as $videoID){
+                           echo "<iframe id='player' type='text/html' width='320' height='200'
+  src='http://www.youtube.com/embed/".$videoID."?enablejsapi=1&origin=http://example.com' frameborder='0'></iframe><br><br>";
+                    }
+                }else{
+                    echo "No Videos<br>Add Videos <a href='index.php?actioncall=addVideos'>Add Videos</a>";
+                }
+            }
+        break;
+    case 'addVideos':
+        $userFullDetails = $_SESSION['account_fulldetails'];
+        $user_id=$userFullDetails['user_id'];
+        $videos = addVideos($client);
+        echo '<pre>';print_r($user_id);exit;
+        $userFullDetails['videoslist']=$videos;
+        $userFullDetails['channel_id']=$videos[0]['id']['channelId'];
+        echo '<pre>';print_r($userFullDetails);exit;
+        if($user_id){
+            $socialDetails = $userObject->addUserVideos($userFullDetails);
+            //print_r($socialDetails);
+        }
         
-        $username = $_POST['username'];
-        
-    }
-    
-    if(isset($_POST['password'])){
-        
-        $password = $_POST['password'];
-        
-    }
-    
-    if(isset($_POST['email'])){
-        
-        $email = $_POST['email'];
-        
-    }
-    
-    $userObject = new User();
-    
-    // Registration
-    
-    if(!empty($username) && !empty($password) && !empty($email)){
-        
-        $hashed_password = md5($password);
-        
-        $json_registration = $userObject->createNewRegisterUser($username, $hashed_password, $email);
-        
-        echo json_encode($json_registration);
-        
-    }
-    
-    // Login
-    
-    if(!empty($username) && !empty($password) && empty($email)){
-        
-        $hashed_password = md5($password);
-        
-        $json_array = $userObject->loginUsers($username, $hashed_password);
-        
-        echo json_encode($json_array);
-    }
-    ?>
+        break;
+        case 'logout':
+            session_destroy();
+            echo "Logout Successfully<br><a href='index.php?actioncall=sociallogin'>Google Login</a>";
+        break;
+    default: echo 'Invalid call';
+        break;
+}
 
-<form id="loginform" method="post">
-Username: <input type="text" name="username" id="username" value="">
-Email <input type="text" name="email" id="email" value="">
-Password: <input type="password" name="password" id="password" value="">
+}else{
+    $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/Login/oauth2callback.php';
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
 
-<input type="submit" name="loginsub" id="loginsub" value="Login">
-</form>
+function addVideos($client){
+    $videoService = new Google_Service_YouTube($client);
+    $channels = $videoService->channels->listChannels('id',array('mine'=>1))->getChannelDetails();
+    //        echo '<pre>';print_r($profileDetails);    
+    foreach($channels as $cId){
+        $channel_id=$cId['id'];
+        $videos = $videoService->search->listSearch('id',array('channelId'=>$channel_id))->getChannelItems();
+    }
+    return $videos;
+}
